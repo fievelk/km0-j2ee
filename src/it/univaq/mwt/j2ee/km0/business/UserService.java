657fd261b88eaf4d50dd1f82104e2067df15866e
Paolo Tatone <paolo.tatone@gmail.com>
2013-05-15 18:26:51 +0200

    Sistemato errore svn di merge su UserService. Cancellato e ricreato.

---
+++ src/it/univaq/mwt/j2ee/km0/business/UserService.java
@@ -0,0 +1,31 @@
+package it.univaq.mwt.j2ee.km0.business;
+
+import java.util.List;
+
+import it.univaq.mwt.j2ee.km0.business.model.Seller;
+import it.univaq.mwt.j2ee.km0.business.model.User;
+
+public interface UserService {
+	
+	void createUser(User user) throws BusinessException;
+	
+	void updateUser(User user) throws BusinessException;
+	
+	void deleteUser(long oid_user) throws BusinessException;
+	
+	List<User> viewAllUsers() throws BusinessException;
+	
+	List<User> getUsersFromPaidCarts() throws BusinessException;
+	
+	void createSeller(Seller seller) throws BusinessException;
+	
+	void updateSeller(Seller seller) throws BusinessException;
+	
+	void deleteSeller(long oid_seller) throws BusinessException;
+	
+	List<Seller> viewAllSellers() throws BusinessException;
+	
+	List<Seller> getSellersFromPaidCarts() throws BusinessException;
+	
+
+}
