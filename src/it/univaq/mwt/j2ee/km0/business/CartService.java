209cc80b156d1fa07579367f3a940c5f8ea20923
Sciamenna <maronno@intrage.it>
2013-05-15 15:18:50 +0200

    CartService aggiornata.

--- src/it/univaq/mwt/j2ee/km0/business/CartService.java
+++ src/it/univaq/mwt/j2ee/km0/business/CartService.java
@@ -1,6 +1,5 @@
 package it.univaq.mwt.j2ee.km0.business;
 
-import java.util.Date;
 import java.util.List;
 
 import it.univaq.mwt.j2ee.km0.business.model.Cart;
@@ -9,15 +8,13 @@ public interface CartService {
 	
 	void createCart(Cart cart);
 	
-	void updateCart(long oid_cart);
+	void updateCart(Cart cart);
 	
 	void deleteCart(long oid_cart);
 	
 	Cart viewCart(long oid_user, long oid_cart);
 	
-	List<Cart> viewOrders(long oid_user);
-	
-	Cart payment(Date paid);
+	List<Cart> viewOrdersUser(long oid_user);
 	
 	List<Cart> viewOrdersSeller(long oid_seller);
 	
