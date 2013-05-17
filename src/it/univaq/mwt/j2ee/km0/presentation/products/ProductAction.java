bef2f64a719d6d39c5d15d01a08220cd23879cfc
fievelk <24alsecondo@gmail.com>
2013-05-17 11:04:48 +0200

    Crea la ProductAction e il forward a una pagina di prova

--- src/it/univaq/mwt/j2ee/km0/presentation/products/ProductAction.java
+++ src/it/univaq/mwt/j2ee/km0/presentation/products/ProductAction.java
@@ -1,7 +1,21 @@
 package it.univaq.mwt.j2ee.km0.presentation.products;
 
-public class ProductAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.MappingDispatchAction;
+
+public class ProductAction extends MappingDispatchAction {
+
+	public ActionForward views(ActionMapping mapping, ActionForm form, 
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		
+		return mapping.findForward("success");
+	}
 	
 	
 }
