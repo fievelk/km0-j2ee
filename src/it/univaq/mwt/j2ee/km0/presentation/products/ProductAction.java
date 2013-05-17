1659bc7da5fb374ad41090ca0144a5d24accbfdc
fievelk <24alsecondo@gmail.com>
2013-05-17 13:11:18 +0200

    crea il metodo insert nella ProductAction

--- src/it/univaq/mwt/j2ee/km0/presentation/products/ProductAction.java
+++ src/it/univaq/mwt/j2ee/km0/presentation/products/ProductAction.java
@@ -1,8 +1,13 @@
 package it.univaq.mwt.j2ee.km0.presentation.products;
 
+import it.univaq.mwt.j2ee.km0.business.KmZeroBusinessFactory;
+import it.univaq.mwt.j2ee.km0.business.model.Product;
+import it.univaq.mwt.j2ee.km0.presentation.products.ProductForm;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -17,5 +22,18 @@ public class ProductAction extends MappingDispatchAction {
 		return mapping.findForward("success");
 	}
 	
+	public ActionForward insert(ActionMapping mapping, ActionForm actionForm, HttpServletRequest req, HttpServletResponse response) throws Exception {
+		ProductForm form = (ProductForm) actionForm;
+		Product product = new Product();
+		BeanUtils.copyProperties(product, form);
+		//ProductKind ProductKind = new ProductKind(form.getProductKindId());
+		//product.setProductKind(ProductKind);
+
+		KmZeroBusinessFactory factory = KmZeroBusinessFactory.getInstance();
+		ProductService service = factory.getProductService();
+
+		service.createProduct(product);
+		return mapping.findForward("success");
+	}
 	
 }
