e44d7fd69d06e05b0264caeff145107e0ef84dc6
fievelk <24alsecondo@gmail.com>
2013-05-17 12:46:28 +0200

    cambia il nome del db nel context listener e il nome della classe (prima c'era un typo: da Listner a Listener)

--- src/it/univaq/mwt/j2ee/km0/presentation/common/KmZeroServletContextListener.java
+++ src/it/univaq/mwt/j2ee/km0/presentation/common/KmZeroServletContextListener.java
@@ -10,14 +10,14 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.sql.DataSource;
 
-public class KmZeroServletContextListner implements ServletContextListener{
+public class KmZeroServletContextListener implements ServletContextListener{
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 		try {
 			Context initCtx = new InitialContext();
 			Context envCtx = ( Context ) initCtx.lookup( "java:comp/env" );
-			DataSource dataSource = ( DataSource ) envCtx.lookup( "jdbc/librarydb" );
+			DataSource dataSource = ( DataSource ) envCtx.lookup( "jdbc/km0db" );
 			KmZeroBusinessFactory.setInstance(new JDBCKmZeroBusinessFactory(dataSource));
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
